6f81d859b8dc70f5a97c05cc6149d610cefa49c3
Xabi Vittorio GOÏTY <xabigoity@gmail.com>
2025-08-07 22:28:30 +0200

    Added root project files

---
+++ app/Root.jsx
@@ -0,0 +1,15 @@
+import "@radix-ui/themes/styles.css";
+
+import { Theme } from "@radix-ui/themes";
+
+export default function () {
+    return (
+        <html>
+        <body>
+        <Theme>
+            <MyApp />
+        </Theme>
+        </body>
+        </html>
+    );
+}
