61bbc01247bed8b6a908470f671c21c71f1c6f4f
Xabi Vittorio GOÏTY <xabigoity@gmail.com>
2025-08-08 00:14:22 +0200

    Desktop navbar

--- src/components/navigation/DesktopLayout.tsx
+++ src/components/navigation/DesktopLayout.tsx
@@ -1,12 +1,21 @@
+import {useEffect, useState} from "react";
+
 export default function DesktopLayout() {
+
+    const [currentRoute, setCurrentRoute] = useState("/");
+
+    useEffect(() => {
+        setCurrentRoute(window.location.pathname);
+    }, [window.location.pathname]);
+
     return (
-        <span>
-            <a href="/">Accueil</a>
-            <a href="/">Présentation</a>
-            <a href="/">Membres</a>
-            <a href="/">Évènements</a>
-            <a href="/">Communauté</a>
-            <a href="/">Nous rejoindre</a>
+        <span className={"p-[15px] py-[15px] h-[35px]"}>
+            <a href="/" className={`p-[5px] navdesktopbtn ${currentRoute == "/" ? "cnavdesktopbtn" : ""}` }>Accueil</a>
+            <a href="/about" className={`p-[5px] navdesktopbtn ${currentRoute == "/about" ? "cnavdesktopbtn" : ""}`}>Présentation</a>
+            <a href="/members" className={`p-[5px] navdesktopbtn ${currentRoute == "/members" ? "cnavdesktopbtn" : ""}`}>Membres</a>
+            <a href="/blog" className={`p-[5px] navdesktopbtn ${currentRoute == "/blog" ? "cnavdesktopbtn" : ""}`}>Blog</a>
+            <a href="/community" className={`p-[5px] navdesktopbtn ${currentRoute == "/community" ? "cnavdesktopbtn" : ""}`}>Communauté</a>
+            <a href="/join" className={`p-[5px] navdesktopbtn ${currentRoute == "/join" ? "cnavdesktopbtn" : ""}`}>Nous rejoindre</a>
         </span>
     );
 }
